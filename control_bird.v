54b7def831673ee3e4a4645988a3a3d14bada449
YifeiGao <52712330+YifeiGao@users.noreply.github.com>
2019-07-10 12:03:06 -0400

    Add files via upload

---
+++ control_bird.v
@@ -0,0 +1,39 @@
+module control_bird(clk, resetn, start, press_key, touched;
+        input clk;
+        input resetn;
+        input press_key;
+        input touched;
+        //output reg start;
+        //output reg move;
+        //current state and next state
+        reg [2:0] current, next;
+        
+        localparam B_READY = 3'b000, B_START = 3'b010, B_RAISING = 3'b110, B_FALLING = 3'b011, B_STOP = 3'b001;
+        
+        always@(*)
+        begin: state_table
+                case(current)
+                        B_READY: next = press_key ? B_START : B_READY;
+                        B_START: next = press_key ? B_RAISING : B_FALLING;
+                        B_RAISING: begin
+                                if(touched) next <= B_STOP;
+                                else next  = press_key ? B_RAISING : B_FALLING;
+                        end
+                        B_FALLING: begin
+                                if(touched) next <= B_STOP;
+                                else next  = press_key ? B_RAISING : B_FALLING;
+                        end
+                        B_STOP: next = B_READY;
+                        default next = B_READY
+                endcase
+        end
+        
+        //state register
+        always@(posedge clk)
+        begin: state_FFS
+                if (!resetn)
+                        current <= S_READY;
+                else
+                        current <= next;
+        end
+endmodule
---
+++ control_wall.v
@@ -0,0 +1,30 @@
+module control_wall(start, touched, clk, resetn);
+    input start;
+    input touched;
+    input clk;
+    input resetn;
+    
+    reg [1:0] current, next;
+    
+    localparam W_READY = 2'b00, W_MOVE = 2'b01; W_STOP = 2'b11;
+    //state table for wall
+    always@(*)
+    begin: state_table
+        case(current)
+            W_READY: next = start ? W_MOVE : W_READY;
+            W_MOVE : next = touched ? W_STOP : W_MOVE;
+            W_STOP: next = W_READY;
+            default next = W_READY;
+        endcase
+    end
+    //state register
+    always@(posedge clk)
+    begin: state_FFS
+        if (!resetn)
+            current <= W_READY;
+        else
+            current <= next;
+    end
+endmodule
+    
+    
