804413b60d7914285f75a78313646c6e07462d5c
YifeiGao <52712330+YifeiGao@users.noreply.github.com>
2019-07-11 12:57:19 -0400

    Update check_touched.v

--- check_touched.v
+++ check_touched.v
@@ -1,13 +1,13 @@
 module check_touched(
-	input bird_xleft;
-	input bird_xright;
-	input bird_ytop;
-	input bird_ybottom;
-	input wall_xleft;
-	input wall_xrigth;
-	input wall_topy;
-	input wall_bottomy;
-	output touched;
+	input bird_xleft,
+	input bird_xright,
+	input bird_ytop,
+	input bird_ybottom,
+	input wall_xleft,
+	input wall_xrigth,
+	input wall_topy,
+	input wall_bottomy,
+	output touched,
 );
 	if (bird_xright >= wall_xleft | bird_xleft <= wall_xright) begin 
 		assign touched  = bird_ytop <= wall_topy | bird_ybottom >= wall_bottomy;
