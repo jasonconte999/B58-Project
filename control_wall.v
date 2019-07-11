85f987f0d8af68915c66add90568d7e63d9c5e36
YifeiGao <52712330+YifeiGao@users.noreply.github.com>
2019-07-11 11:19:12 -0400

    Update control_wall.v

--- control_wall.v
+++ control_wall.v
@@ -1,17 +1,20 @@
-module control_wall(start, touched, clk, resetn);
-    input start;
+module control_wall(go, touched, clk, resetn, start, move);
+    input go;
     input touched;
     input clk;
     input resetn;
+    output reg start;
+    output reg move;
     
-    localparam W_READY = 2'b00, W_MOVE = 2'b01; W_STOP = 2'b11;    reg [1:0] current, next;
+    localparam W_READY = 2'b00, W_MOVE = 2'b01; W_STOP = 2'b11;    
+    reg [1:0] current, next;
     
     localparam W_READY = 2'b00, W_MOVE = 2'b01, W_STOP = 2'b11;
     //state table for wall
     always@(*)
     begin: state_table
         case(current)
-            W_READY: next = start ? W_MOVE : W_READY;
+            W_READY: next = go ? W_MOVE : W_READY;
             W_MOVE : next = touched ? W_STOP : W_MOVE;
             W_STOP: next = W_READY;
             default next = W_READY;
