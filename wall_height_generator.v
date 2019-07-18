8434eec616f45edf89318c70f088d6787230794a
ytwwww <52704719+ytwwww@users.noreply.github.com>
2019-07-18 07:40:56 -0400

    Update and rename Random_gen.v to wall_height_generator.v

---
+++ wall_height_generator.v
@@ -0,0 +1,38 @@
+// use a linear feedback shift register to generate nearly random numbers
+// the output then be used to determine the height of the wall in game
+module wall_height_generator(clk, reset, out)
+    input clk, reset;
+    output reg [7:0] out;
+    // xor 2nd and 4th bits as feedback
+    wire feedback = out[2] ^ out[4];
+    
+    reg [4:0] number, number_nxt, random_number;
+    // record number of shifts
+    reg [3:0] shift, shift_nxt
+    
+    always @ (posedge clock or negedge resetn)
+    begin
+        if (~reset) 
+            number <= 5'hF;
+            shift <= 0;
+        else
+            number <= number_nxt;
+            shift <= shift_nxt;
+    end
+    
+    always @ (*)
+    begin
+        number_nxt = number;//
+        shift_nxt = shift;
+        
+        number_nxt = {out[3:0], feedback};
+        shift_nxt = shift + 1;
+        
+        // after all shifts done, we get a random number
+        if (shift == 5)
+            shift = 0;
+            random_number = number;
+    end
+    
+    assign out = random_number * 3;
+endmodule
