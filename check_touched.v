52e8783d3dd20ce8839d124fa6ac0a650438e634
ytwwww <52704719+ytwwww@users.noreply.github.com>
2019-07-21 17:12:46 -0400

    Update check_touched.v

--- check_touched.v
+++ check_touched.v
@@ -2,8 +2,7 @@ module check_touched(
 	input [7:0] bird_xleft, bird_xright, bird_ytop, bird_ybottom, wall_xleft, wall_xright, wall_topy, wall_bottomy,
 	output touched
 );
-	wire in_the_hole;
-	wire touched_top_bottom;
+	wire in_the_hole, touched_top_bottom;
 	// check x coordinate: left and right positions of bird and wall
 	assign in_the_hole = (bird_xright >= wall_xleft) && (bird_xleft <= wall_xright);
 	// check y coordinate: top and bottom positions of bird and wall
