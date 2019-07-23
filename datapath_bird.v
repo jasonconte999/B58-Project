2a1e1312ecad39921e5bb439d79199219bdab5d3
AdilShah1 <52712524+AdilShah1@users.noreply.github.com>
2019-07-23 11:55:25 -0400

    Add files via upload
    fixed bugs

--- datapath_bird.v
+++ datapath_bird.v
@@ -1,24 +1,17 @@
-module datapath_bird(
-	input clk,
-	input [1:0] cur_state,
-	output y_out
-    );
+module datapath_bird(input clk, input [1:0] cur_state, output y_out);
     
     // registers
-    reg [7:0] bird_vy, bird_y, score;
-	reg [7:0] draw_x, draw_y;
+    reg [7:0] bird_vy, bird_y;
 
 	localparam BIRD_Y_START = 8'b00111100; //Bird starts at y=60
 	localparam BIRD_VY_START = 8'b00000000; //Bird's vertical speed starts at 0
 	localparam BIRD_VY_SPEED = 8'b00000100; //Bird's vertical speed decreases by speed 4
 	localparam BIRD_VY_JUMP = 8'b00001010; //When bird jumps, velocity becomes 10
 	
-	localparam BACKGROUND_COLOUR = 3'b111;
 	localparam GRAVITY = 8'b0000001000;
 
 	localparam UPDATE_BIRD_Y = 6'd0,
-		UPDATE_BIRD_VY = 6'd1,
-		DEL_BIRD = 6'd2;
+				  UPDATE_BIRD_VY = 6'd1;
 
     // State mapping
     always @(*)
@@ -34,5 +27,7 @@ module datapath_bird(
 			end
         endcase
     end
+	 
+	 assign y_out = bird_y;
     
 endmodule
