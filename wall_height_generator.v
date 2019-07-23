45eb40e0e35e71b30fcdad84c0cf2991737f4a06
ytwwww <52704719+ytwwww@users.noreply.github.com>
2019-07-23 07:24:45 -0400

    Update wall_height_generator.v

--- wall_height_generator.v
+++ wall_height_generator.v
@@ -3,7 +3,7 @@
 module wall_height_generator(clk, resetn, out);
     input clk, resetn;
     output wire [7:0] out;
-    // xor 1st and 4th bits as feedback
+    // xor 2nd and 4th bits as feedback
     wire feedback = number[1] ^ number[4];
     
     reg [4:0] number, number_nxt, random_number;
@@ -12,10 +12,10 @@ module wall_height_generator(clk, resetn, out);
 	 
     always @ (posedge clk or negedge resetn)
     begin
-	if (~resetn) begin
+        if (~resetn) begin
             number <= 5'hF;
             shift <= 0;
-	end
+				end
         else
             number <= number_nxt;
             shift <= shift_nxt;
@@ -34,3 +34,6 @@ module wall_height_generator(clk, resetn, out);
             shift <= 0;
             random_number <= number;
     end
+    
+    assign out = (random_number * 3);
+endmodule
