9106674d5ebee9cb31ea786ae28fec9aacbb86bb
ytwwww <52704719+ytwwww@users.noreply.github.com>
2019-07-23 13:26:40 -0400

    Update control_bird.v

--- control_bird.v
+++ control_bird.v
@@ -3,19 +3,21 @@ module control_bird(clk, resetn, press_key, touched, current);
         input resetn;
         input press_key;
         input touched;
+        input flag; // whether bird is too high
         output reg current;
         //current state and next state
         reg next, afterDraw;
         
-        localparam B_READY = 3'b000, B_START = 3'b010, B_RAISING = 3'b110, B_FALLING = 3'b011, B_STOP = 3'b001, B_DRAW = 111;
+        localparam /*B_READY = 3'b000,*/ B_START = 3'b010, B_RAISING = 3'b110, B_FALLING = 3'b011, B_STOP = 3'b001, B_DRAW = 111;
         
         always@(*)
         begin: state_table
                 case(current)
+                        /*
                         B_READY: begin
                                 afterDraw = press_key ? B_START : B_READY;
                                 next = B_DRAW;
-                        end
+                        end*/
                         B_START: begin
                                 afterDraw = press_key ? B_RAISING : B_FALLING;
                                 next = B_DRAW;
@@ -23,19 +25,20 @@ module control_bird(clk, resetn, press_key, touched, current);
                         B_RAISING: begin
                                 if (touched) afterDraw <= B_STOP;
                                 else afterDraw  = press_key ? B_RAISING : B_FALLING;
+                                //else afterDraw  = press_key ? B_RAISING : B_FALLING;
                                 next = B_DRAW;
                         end
                         B_FALLING: begin
                                 if (touched) afterDraw <= B_STOP;
-                                else afterDraw  = press_key ? B_RAISING : B_FALLING;
+                                else afterDraw  = flag ? B_RAISING : B_FALLING;
                                 next = B_DRAW;
                         end
                         B_STOP: begin
-                                afterDraw = B_READY;
+                                afterDraw = B_START;
                                 next = B_DRAW;
                         end
                         B_DRAW: next = afterDraw;
-                        default next = B_READY;
+                        default next = B_START;
                 endcase
         end
         
@@ -57,7 +60,7 @@ module control_bird(clk, resetn, press_key, touched, current);
         always@(posedge clk)
         begin: state_FFS
                 if (!resetn)
-                        current <= B_READY;
+                        current <= B_START;
                 else
                         current <= next;
         end
