076beb2e955f23f393257360446b90dce5954f1f
AdilShah1 <52712524+AdilShah1@users.noreply.github.com>
2019-07-23 11:22:46 -0400

    Add files via upload
    fixed bug

--- datapath_wall.v
+++ datapath_wall.v
@@ -1,8 +1,6 @@
-module datapath_wall(input clk, input [1:0] cur_state, output [7:0] x_out, output [7:0] y_out);
+module datapath_wall(input clk, input [1:0] cur_state, output [7:0] x_out);
    // registers
-	reg [7:0] wall_x, hole_y;
-	
-	wire [7:0] out;
+	reg [7:0] wall_x;
 	
 	localparam WALL_X_SPEED = 8'b00000100;//Speed of wall is 4
 	localparam WALL_HEIGHT = 8'b01111000;//height of wall is 120
@@ -23,12 +21,10 @@ module datapath_wall(input clk, input [1:0] cur_state, output [7:0] x_out, outpu
 			UPDATE_WALL:
 				begin
 					wall_x <= wall_x - WALL_X_SPEED;
-					hole_y <= out;
 				end
 		endcase
    end
    
 	assign x_out = wall_x;
-	assign y_out = hole_y;
 
 endmodule	
