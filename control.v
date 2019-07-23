7b39bba2cea289d58a7aee04cc866cc83f0c095d
ytwwww <52704719+ytwwww@users.noreply.github.com>
2019-07-23 11:41:29 -0400

    Update control.v

--- control.v
+++ control.v
@@ -1,8 +1,9 @@
-module control(clk, resetn, go, touched, current, bird_curr, wall_curr);
+module control(clk, resetn, go, touched, finished_draw, bird_curr, wall_curr);
         input clk, resetn, go, collision;
-        output reg current, bird_curr, wall_curr;
+        output reg finished_draw, bird_curr, wall_curr;
+	reg next;
         
-	localparam BIRD = , WALL = ;
+	localparam DRAW_WALL = 2'b00, DRAW_BIRD = 2'b01;
 	
         control_bird bird_controller(
                 .clk(clk),
@@ -20,3 +21,21 @@ module control(clk, resetn, go, touched, current, bird_curr, wall_curr);
 		.current(wall_curr)
 	      );
   
+	always@(*)
+        begin: state_table
+                case(finished_draw)
+			DRAW_WALL: next = DRAW_BIRD;
+			DRAW_BIRD: next = ;
+                        default next = DRAW_WALL;
+                endcase
+        end
+        
+        //state register
+        always@(posedge clk)
+        begin: state_FFS
+                if (!resetn)
+                        finished_draw <= DRAW_WALL;
+                else
+                        finished_draw <= next;
+        end
+endmodule
