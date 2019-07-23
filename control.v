212986549c6fc54d5804f6b4da990ba4c6fd5be0
ytwwww <52704719+ytwwww@users.noreply.github.com>
2019-07-23 12:19:31 -0400

    Update control.v

--- control.v
+++ control.v
@@ -1,6 +1,6 @@
-module control(clk, resetn, go, touched, finished_draw, bird_curr, wall_curr);
+module control(clk, resetn, go, touched, cur_state, bird_curr, wall_curr);
         input clk, resetn, go, collision;
-        output reg finished_draw, bird_curr, wall_curr;
+        output reg cur_state, bird_curr, wall_curr;
 	reg next;
         
 	localparam DRAW_WALL = 2'b00, DRAW_BIRD = 2'b01;
@@ -25,7 +25,9 @@ module control(clk, resetn, go, touched, finished_draw, bird_curr, wall_curr);
         begin: state_table
                 case(finished_draw)
 			DRAW_WALL: next = DRAW_BIRD;
+			cur_state = wall_curr;
 			DRAW_BIRD: next = ;
+			cur_state = bird_curr;
                         default next = DRAW_WALL;
                 endcase
         end
@@ -34,8 +36,8 @@ module control(clk, resetn, go, touched, finished_draw, bird_curr, wall_curr);
         always@(posedge clk)
         begin: state_FFS
                 if (!resetn)
-                        finished_draw <= DRAW_WALL;
+                        cur_state <= DRAW_WALL;
                 else
-                        finished_draw <= next;
+                        cur_state <= next;
         end
 endmodule
