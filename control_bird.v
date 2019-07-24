a860438c2d7e334345aa71bcc584893290b1dd6b
ytwwww <52704719+ytwwww@users.noreply.github.com>
2019-07-24 00:06:08 -0400

    Update control_bird.v

--- control_bird.v
+++ control_bird.v
@@ -4,11 +4,11 @@ module control_bird(clk, resetn, flag, press_key, touched, current);
         input press_key;
         input touched;
         input flag; // whether bird is too high
-        output reg current;
+        output reg [2:0] current;
         //current state and next state
-        reg next, afterDraw;
+        reg [2:0] next, afterDraw;
         
-        localparam B_START = 3'b010, B_RAISING = 3'b110, B_FALLING = 3'b011, B_STOP = 3'b001, B_DRAW = 111; /*B_READY = 3'b000,*/
+        localparam B_START = 3'b001, B_RAISING = 3'b010, B_FALLING = 3'b011, B_STOP = 3'b100, B_DRAW = 3'b111; /*B_READY = 3'b000,*/
         
         always@(*)
         begin: state_table
@@ -19,26 +19,26 @@ module control_bird(clk, resetn, flag, press_key, touched, current);
                                 next = B_DRAW;
                         end*/
                         B_START: begin
-                                afterDraw = press_key ? B_RAISING : B_START;
-                                next = B_DRAW;
+                                afterDraw <= press_key ? B_RAISING : B_START;
+                                next <= B_DRAW;
                         end
                         B_RAISING: begin
                                 if (touched) afterDraw <= B_STOP;
-                                else afterDraw  = flag ? B_FALLING : B_RAISING;
+                                else afterDraw  <= flag ? B_FALLING : B_RAISING;
                                 //else afterDraw  = press_key ? B_RAISING : B_FALLING;
-                                next = B_DRAW;
+                                next <= B_DRAW;
                         end
                         B_FALLING: begin
                                 if (touched) afterDraw <= B_STOP;
-                                else afterDraw  = press_key ? B_RAISING : B_FALLING;
-                                next = B_DRAW;
+                                else afterDraw  <= press_key ? B_RAISING : B_FALLING;
+                                next <= B_DRAW;
                         end
                         B_STOP: begin
-                                afterDraw = B_START;
-                                next = B_DRAW;
+                                afterDraw <= B_START;
+                                next <= B_DRAW;
                         end
-                        B_DRAW: next = afterDraw;
-                        default next = B_START;
+                        B_DRAW: next <= afterDraw;
+                        default next <= B_START;
                 endcase
         end
         
