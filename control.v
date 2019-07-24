5e1fe1851ac911f356fcaba2eaf90fa8687e80c0
ytwwww <52704719+ytwwww@users.noreply.github.com>
2019-07-24 00:54:27 -0400

    Update control.v

--- control.v
+++ control.v
@@ -1,14 +1,14 @@
-module control(clk, resetn, go, touched, flag, cur_state, bird_curr, wall_curr);
-        input clk, resetn, collision, flag;
-	input reg go;
+module control(clk, resetn, go, touched, flag, collision, cur_state, bird_curr, wall_curr);
+   input clk, resetn, touched, flag, collision;
+	input reg go; //error
 	output reg [2:0] cur_state, bird_curr, wall_curr;
 	reg [2:0] next;
-        
+	 
 	localparam WALL = 3'b000, BIRD = 3'b001; // BACKGROUND = 3'b10,
 	
         control_bird bird_controller(
                 .clk(clk),
-		.flag(flag),
+					 .flag(flag),
                 .resetn(resetn),
                 .press_key(go),
                 .touched(collision),
@@ -25,29 +25,29 @@ module control(clk, resetn, go, touched, flag, cur_state, bird_curr, wall_curr);
   
 	always@(*)
         begin: state_table
-                case(cur_state)
+        case(cur_state)
 			/*
 			BACKGROUND: begin
 				cur_state = ;
 				next = WALL;
 			*/
 			WALL: begin
-				cur_state = wall_curr;
-				next = BIRD;
+				cur_state <= wall_curr;
+				next <= BIRD;
 			end
 			BIRD: begin 
-				cur_state = bird_curr;
-				next = WALL;
+				cur_state <= bird_curr;
+				next <= WALL;
 			end
-                        default next = WALL;
-                endcase
+			default next <= WALL;
+        endcase
         end
         
         //state register
-        always@(posedge clk)
+    always@(posedge clk)
         begin: state_FFS
                 if (!resetn)
-                        cur_state <= WALL;
+                        cur_state <= WALL; //error
                 else
                         cur_state <= next;
         end
