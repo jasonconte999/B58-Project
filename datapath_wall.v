95cabc6faca6229ca5142b401ec6edda7bb41e5e
AdilShah1 <52712524+AdilShah1@users.noreply.github.com>
2019-07-25 16:46:37 -0400

    Add files via upload
    need to generate new wall

--- datapath_wall.v
+++ datapath_wall.v
@@ -1,9 +1,12 @@
 module datapath_wall(input clk, input [1:0] cur_state, output [7:0] x_out);
-   // registers
-	reg [7:0] wall_x = 8'd160;//wall starts at 160
 	
-	localparam WALL_X_SPEED = 8'd4;//Speed of wall is 4
+	localparam WALL_X_SPEED = 8'b00000100;//Speed of wall is 4
 	localparam WALL_HEIGHT = 8'b01111000;//height of wall is 120
+	localparam WALL_START = 8'b10100000;//wall starts at 160
+	//localparam HOLE_HEIGHT = 8'b00110010; //Hole is 50 pixels high
+	
+	// registers
+	reg [7:0] wall_x = 8'd0;// WALL_START;
 	
 	localparam 	UPDATE_WALL = 3'd0;
 	
@@ -13,11 +16,13 @@ module datapath_wall(input clk, input [1:0] cur_state, output [7:0] x_out);
 		case (cur_state)
 			UPDATE_WALL:
 				begin
+					if(wall_x <= 0)
+						wall_x <= WALL_START;
 					wall_x <= wall_x - WALL_X_SPEED;
 				end
 		endcase
    end
-   
+	
 	assign x_out = wall_x;
 
 endmodule	
