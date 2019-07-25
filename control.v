68cecd345b096093c4996d9d9c97519b8067a9cc
jasonconte999 <39284732+jasonconte999@users.noreply.github.com>
2019-07-25 14:17:56 -0400

    Update control.v

--- control.v
+++ control.v
@@ -7,6 +7,7 @@ module control(SW, LEDR, KEY);
 		.clk(KEY[3]),
 		.resetn(SW[0]),
 		.go(SW[1]),
+		.touched(SW[4]),
 		.flag(SW[2]),
 		.collision(SW[3]),
 		.bird_curr(LEDR[3:0]),
@@ -16,7 +17,7 @@ module control(SW, LEDR, KEY);
 endmodule
 
 module control2(clk, resetn, go, touched, flag, collision, cur_state, bird_curr, wall_curr);
-	input clk, resetn, flag, collision, go;
+	input clk, resetn, touched, flag, collision, go;
 	output reg [3:0] cur_state;
 	output [3:0] bird_curr, wall_curr;
 	reg [3:0] next, cur_par_state;
@@ -27,20 +28,25 @@ module control2(clk, resetn, go, touched, flag, collision, cur_state, bird_curr,
 		if (go == 1'b0) gogo <= 1'b1;
 	end
 	//localparam WALL = 4'b1111, BIRD = 4'b1110;
+	/*
 	control_bird bird_controller(
 		.clk(clk),
 		.flag(flag),
 		.resetn(resetn),
 		.press_key(gogo),
 		.touched(collision),
-		.current(bird_curr));
+		.current(bird_state_out));
 	
 	control_wall wall_controller(
 		.go(gogo),
 		.touched(collision),
 		.clk(clk),
 		.resetn(resetn),
-		.current(wall_curr));
+		.current(wall_state_out));
+	*/
+	wire [3:0] bird_state_out, wall_state_out;
+	assign bird_state_out = 4'b1001;
+	assign wall_state_out = 4'b1011;
 	
 	always@(posedge clk)
         begin: state_table
@@ -52,13 +58,13 @@ module control2(clk, resetn, go, touched, flag, collision, cur_state, bird_curr,
 			*/
 			4'b0000: begin
 				cur_par_state <= 4'b0001;
-				next <= bird_curr;
+				next <= bird_state_out;
 			end
 			4'b0001: begin 
 				cur_par_state <= 4'b0000;
-				next <= wall_curr;
+				next <= wall_state_out;
 			end
-			default next <= wall_curr;
+			default next <= wall_state_out;
         endcase
         end
         
@@ -66,8 +72,11 @@ module control2(clk, resetn, go, touched, flag, collision, cur_state, bird_curr,
     always@(posedge clk)
         begin: state_FFS
                 if (!resetn)
-                        cur_state <= wall_curr;
+                        cur_state <= wall_state_out;
                 else
                         cur_state <= next;
         end
+		  
+	assign bird_curr = bird_state_out;
+	assign wall_curr = wall_state_out;
 endmodule
