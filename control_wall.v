01801ae0123ac9551138d22e49bdbeb5c1bab52e
ytwwww <52704719+ytwwww@users.noreply.github.com>
2019-07-26 16:12:16 -0400

    Update control_wall.v

--- control_wall.v
+++ control_wall.v
@@ -5,7 +5,7 @@ module control_wall(go, touched, clk, current_out);
     output [3:0] current_out;
     
     localparam W_READY = 4'b0101, W_MOVE = 4'b0110, W_STOP = 4'b0111, W_DRAW = 4'b1000, W_DEL = 4'b1001, W_UPDATE = 4'b1010;
-    reg [3:0] next, afterDraw, current;
+    reg [3:0] afterDraw, current;
 
     
     // state table for wall
@@ -14,16 +14,14 @@ module control_wall(go, touched, clk, current_out);
         case(current)
             W_READY: begin
                 afterDraw = go ? W_MOVE : W_READY; // move until the go is high
-                current = W_DRAW;
+                current = W_DEL;
             end
             W_MOVE : begin 
                 afterDraw = touched ? W_STOP : W_MOVE; // stop if it's touched
-                current = W_DRAW;
+                current = W_DEL;
             end
             W_STOP: begin
-                //afterDraw = W_READY;
-                //next = W_DRAW;
-					if (touched) current = W_READY;
+		if (touched) current = W_READY;
             end
 	    W_DEL: current = W_UPDATE;
 	    W_UPDATE: current =  W_DRAW;
@@ -33,18 +31,6 @@ module control_wall(go, touched, clk, current_out);
     end
 	 
 	 assign current_out = current;
-    /*
-    //state table for wall
-    always@(*)
-    begin: state_table
-        case(current)
-            W_READY: next = go ? W_MOVE : W_READY; //move until the go is high
-            W_MOVE : next = touched ? W_STOP : W_MOVE; //stop if it's touched
-            W_STOP: next = W_READY;
-            default next = W_READY;
-        endcase
-    end
-    */
     
     /*
     //enable signals
